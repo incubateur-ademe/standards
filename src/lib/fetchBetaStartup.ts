e1b8e74190b550ae6ed7e55858f2861a79194867
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-12-11 20:38:13 +0100

    fix(ci): commit generated prisma client

--- src/lib/fetchBetaStartup.ts
+++ src/lib/fetchBetaStartup.ts
@@ -1,5 +1,32 @@
 import { config } from "@/config";
-import { type BetaGouvStartup } from "@/startup-types";
+
+export interface BetaGouvStartup {
+  accessibility_status: string;
+  active_members: string[];
+  budget_url: string | null;
+  contact: string;
+  content: string;
+  dashlord_url: string | null;
+  expired_members: string[];
+  id: string;
+  impact_url: string | null;
+  incubator: string;
+  link: string;
+  mission: string;
+  mon_service_securise: boolean;
+  name: string;
+  phases: Array<{
+    name: string;
+    start: string; // ISO date
+  }>;
+  previous_members: string[];
+  redirect_from: string | null;
+  repository: string;
+  screenshot: string;
+  stats_url: string | null;
+  techno: string[];
+  usertypes: string[];
+}
 
 export const fetchBetaStartup = async (id: string) => {
   const res = await fetch(`${config.betaGouvUrl}/api/v3/startups/${id}.json`, {
