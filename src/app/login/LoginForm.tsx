a5ed8cda11d642f37335c0c1bde1fed9d8512619
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-12-24 14:20:04 +0100

    fix login

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -2,28 +2,23 @@ import ButtonsGroup from "@codegouvfr/react-dsfr/ButtonsGroup";
 import Input from "@codegouvfr/react-dsfr/Input";
 import { ESPACE_MEMBRE_PROVIDER_ID } from "@incubateur-ademe/next-auth-espace-membre-provider";
 import { EspaceMembreClientMemberNotFoundError } from "@incubateur-ademe/next-auth-espace-membre-provider/EspaceMembreClient";
-import { redirect, unstable_rethrow as rethrow } from "next/navigation";
 import { AuthError } from "next-auth";
+import { redirect, unstable_rethrow as rethrow } from "next/navigation";
 
 import { FormFieldset } from "@/dsfr";
 import { signIn } from "@/lib/next-auth/auth";
 import { isRedirectError, type NextError } from "@/utils/next";
 
-export interface LoginFormProps {
-  domain?: string;
-  loginWithEmail?: boolean;
-}
-
 const loginValueKey = "login";
 
-export const LoginForm = ({ loginWithEmail }: LoginFormProps) => {
+export const LoginForm = () => {
   return (
     <form
       action={async data => {
         "use server";
 
         try {
-          await signIn(loginWithEmail ? "nodemailer" : ESPACE_MEMBRE_PROVIDER_ID, {
+          await signIn(ESPACE_MEMBRE_PROVIDER_ID, {
             email: data.get(loginValueKey),
             redirectTo: "/",
           });
@@ -40,38 +35,20 @@ export const LoginForm = ({ loginWithEmail }: LoginFormProps) => {
       }}
     >
       <FormFieldset
-        legend={
-          <h2>
-            {loginWithEmail
-              ? "Se connecter avec son adresse mail"
-              : "Se connecter avec son nom d'utilisateur beta gouv"}
-          </h2>
-        }
+        legend={<h2>Se connecter avec son nom d'utilisateur beta gouv</h2>}
         elements={[
-          loginWithEmail ? (
-            <Input
-              key="email"
-              label="Email"
-              nativeInputProps={{
-                type: "email",
-                required: true,
-                name: loginValueKey,
-              }}
-            />
-          ) : (
-            <Input
-              key="username"
-              label="Identifiant"
-              nativeInputProps={{
-                type: "text",
-                required: true,
-                name: loginValueKey,
-                pattern: "^[A-Za-z.]+$",
-                title:
-                  "Votre identifiant doit être composé de lettres et de points. Il ne s'agit pas de votre adresse email Beta.",
-              }}
-            />
-          ),
+          <Input
+            key="username"
+            label="Identifiant"
+            nativeInputProps={{
+              name: loginValueKey,
+              pattern: "^[A-Za-z.]+$",
+              required: true,
+              title:
+                "Votre identifiant doit être composé de lettres et de points. Il ne s'agit pas de votre adresse email Beta.",
+              type: "text",
+            }}
+          />,
           <FormFieldset
             key="submit"
             legend={null}
